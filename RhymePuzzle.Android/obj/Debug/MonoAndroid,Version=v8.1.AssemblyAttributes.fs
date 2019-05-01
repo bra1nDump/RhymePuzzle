4790e19ee95baa4d7d084578f06d1a66c1364a49
Kirill Dubovitskiy <kirill@padx.com>
2019-04-30 21:48:46 -0700

    removed all **/obj/* files

--- RhymePuzzle.Android/obj/Debug/MonoAndroid,Version=v8.1.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute("MonoAndroid,Version=v8.1", FrameworkDisplayName="Xamarin.Android v8.1 Support")>]
-                do ()
